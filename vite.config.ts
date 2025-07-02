0bb73e47f6214ed3415c60a50582ba876486a4ff
nXs <nxs88@mail.ru>
2025-07-03 05:04:25 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Vegetable-Store/',
+  base: '/Kata-Vegetable-Store/',
   plugins: [react()],
   test: {
     setupFiles: ['./setupTests.ts'],
